9021e49ed08db19f89072dc7e36e097a372888f3
Tojeiro-Casa <adantjm@gmail.com>
2018-04-05 02:58:59 +0200

    @Version D.0.2
    Refactorizacion commit

---
+++ Proyecto 3/src/principal/Launcher.java
@@ -0,0 +1,29 @@
+package principal;
+
+import java.awt.EventQueue;
+
+import interfaz.Ventana;
+
+public class Launcher {
+	/*
+	 * Autor: Adan Jarillo Merida
+	 * Version: en Desarrollo 0.2
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana window = new Ventana();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
