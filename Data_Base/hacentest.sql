3013fb465193d5fceff57f977473ec0bb1673f54
Tojeiro-Casa <adantjm@gmail.com>
2018-04-20 03:37:28 +0200

    @!B.B.D.D. Alpha 1.0.0
    Añadida primer esquema de la base de datos, añadido tambien un apartado en el github  para la documentacion

--- Data_Base/hacentest.sql
+++ Data_Base/hacentest.sql
@@ -1,29 +1,99 @@
--- MySQL dump 10.16  Distrib 10.1.31-MariaDB, for Win32 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: hacentest
--- ------------------------------------------------------
--- Server version	10.1.31-MariaDB
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-04-2018 a las 03:33:59
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `usuario`
+-- Base de datos: `hacentest`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `enuncian`
+--
+
+CREATE TABLE `enuncian` (
+  `fk_test` int(11) NOT NULL,
+  `fk_pregunta` int(11) NOT NULL,
+  `respuesta` char(1) COLLATE latin1_spanish_ci NOT NULL,
+  `resultado` varchar(20) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pregunta`
+--
+
+CREATE TABLE `pregunta` (
+  `pk_pregunta` int(11) NOT NULL,
+  `enunciado` varchar(500) COLLATE latin1_spanish_ci NOT NULL,
+  `indice_correcto` char(1) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta`
+--
+
+CREATE TABLE `respuesta` (
+  `pk_respuesta` int(11) NOT NULL,
+  `texto` varchar(300) COLLATE latin1_spanish_ci NOT NULL,
+  `indice` char(1) COLLATE latin1_spanish_ci NOT NULL,
+  `fk_pregunta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sesion`
+--
+
+CREATE TABLE `sesion` (
+  `pk_sesion` int(11) NOT NULL,
+  `tiempo_inicio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tiempo_final` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fk_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `test`
+--
+
+CREATE TABLE `test` (
+  `pk_test` int(11) NOT NULL,
+  `tiempo_inicio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tiempo_final` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fk_sesion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
 CREATE TABLE `usuario` (
-  `pk_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `pk_usuario` int(11) NOT NULL,
   `nick` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
   `pass` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
   `acceso` int(11) NOT NULL DEFAULT '0',
@@ -32,27 +102,117 @@ CREATE TABLE `usuario` (
   `dni` varchar(9) COLLATE latin1_spanish_ci NOT NULL,
   `email` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
   `fecha_registro` date NOT NULL,
-  `fecha_ultima_sesion` date NOT NULL,
-  PRIMARY KEY (`pk_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `estado` varchar(20) COLLATE latin1_spanish_ci NOT NULL DEFAULT 'offline'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
--- Dumping data for table `usuario`
+-- Índices para tablas volcadas
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indices de la tabla `enuncian`
+--
+ALTER TABLE `enuncian`
+  ADD KEY `fk_pregunta` (`fk_pregunta`),
+  ADD KEY `fk_test` (`fk_test`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indices de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`pk_pregunta`);
+
+--
+-- Indices de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD PRIMARY KEY (`pk_respuesta`),
+  ADD KEY `fk_pregunta` (`fk_pregunta`);
+
+--
+-- Indices de la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  ADD PRIMARY KEY (`pk_sesion`),
+  ADD KEY `fk_usuario` (`fk_usuario`);
+
+--
+-- Indices de la tabla `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`pk_test`),
+  ADD KEY `fk_sesion` (`fk_sesion`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`pk_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  MODIFY `pk_pregunta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  MODIFY `pk_respuesta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  MODIFY `pk_sesion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `test`
+--
+ALTER TABLE `test`
+  MODIFY `pk_test` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `pk_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `enuncian`
+--
+ALTER TABLE `enuncian`
+  ADD CONSTRAINT `enuncian_ibfk_1` FOREIGN KEY (`fk_pregunta`) REFERENCES `pregunta` (`pk_pregunta`),
+  ADD CONSTRAINT `enuncian_ibfk_2` FOREIGN KEY (`fk_test`) REFERENCES `test` (`pk_test`);
+
+--
+-- Filtros para la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`fk_pregunta`) REFERENCES `pregunta` (`pk_pregunta`);
+
+--
+-- Filtros para la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  ADD CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`pk_usuario`);
+
+--
+-- Filtros para la tabla `test`
+--
+ALTER TABLE `test`
+  ADD CONSTRAINT `test_ibfk_1` FOREIGN KEY (`fk_sesion`) REFERENCES `sesion` (`pk_sesion`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-04-09  4:38:12
